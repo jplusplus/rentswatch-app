a826fcc85fb8c6622993218ffe6bb530019f86e7
pirhoo <hello@pirhoo.com>
2016-01-08 14:27:26 +0100

    Remove png from rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,6 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
-            '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/public/assets/fonts/*'
           ]
         }
