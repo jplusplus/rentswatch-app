cdfe92c1f5524867f0ba35dd4432b38bb526cc42
pirhoo <hello@pirhoo.com>
2016-03-23 18:44:53 +0100

    Added prerendering

--- server/config/express.js
+++ server/config/express.js
@@ -14,6 +14,7 @@ var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
+var prerender = require('prerender-node');
 
 module.exports = function(app) {
   var env = app.get('env');
@@ -25,7 +26,8 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
-  
+  app.use(prerender);
+
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
